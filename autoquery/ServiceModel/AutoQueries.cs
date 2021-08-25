5cf3d836d320814da5f1be202b8f831f628c63b5
Jens Petter Abrahamsen <jens.abrahamsen@basefarm.com>
2021-08-25 13:34:37 +0200

    SS updated, works now

--- autoquery/ServiceModel/AutoQueries.cs
+++ autoquery/ServiceModel/AutoQueries.cs
@@ -9,7 +9,7 @@ namespace autoquery.ServiceModel
         public class QueryEmployee : QueryDb<Employee> {
             // explicitly adding this will remove any warning about "missing property"
             // (as AutoQuery adds properties on-the-fly, properties not explicitly listed here will give a warning, which is problematic, if you follow up logs, looking for _real_ warnings/errors)
-            public DateTime? BirthDateGreaterThan { get; set; }     
+//            public DateTime? BirthDateGreaterThan { get; set; }     // no longer necessary 5.12.1 (redownload if you need to) -- fixed my @mythz
         }
     }
 }
