0a3024f09d5343347b03281b0dd9138d653aa09e
Jens Petter Abrahamsen <jens.abrahamsen@basefarm.com>
2021-08-25 12:07:31 +0200

    explicit BirthDate property on AutoQuery DTO

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 This is just used for a bug report. (I think it's a bug at least)
-
-I did not include the Northwind database, as it just makes the repo bigger.
-Get it with:
-
-    x mix northwind.sqlite
-
--- autoquery/ServiceModel/AutoQueries.cs
+++ autoquery/ServiceModel/AutoQueries.cs
@@ -1,10 +1,13 @@
 ﻿using ServiceStack;
+using System;
 
 namespace autoquery.ServiceModel
 {
     public class AutoQueries
     {
         [Route("/employees")]
-        public class QueryEmployee : QueryDb<Employee> { }
+        public class QueryEmployee : QueryDb<Employee> {
+            public DateTime? BirthDate { get; set; }
+        }
     }
 }
